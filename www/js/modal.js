fb51e5d62f875a92766b7f3cdd815917b65585ed
?????? ??????? <nikita@infolio.ru>
2013-07-07 20:03:08 +0400

    ??????? ? ????????? ????: ????? ????????.

--- www/js/modal.js
+++ www/js/modal.js
@@ -20,34 +20,20 @@ var modal = (function () {
 		gallery: {
 			html:
 				'<div class="b-modal-gallery"> \
-					<div class="slide prev-prev"> \
-						<img class="image"/> \
-					</div> \
-					<div class="slide prev"> \
-						<img class="image"/> \
-					</div> \
-					<div class="slide current"> \
-						<img class="image"/> \
-					</div> \
-					<div class="slide next"> \
-						<img class="image"/> \
-					</div> \
-					<div class="slide next-next"> \
-						<img class="image"/> \
-					</div> \
+					<a class="b-close-modal" href="?"> \
+						<b class="b-icon b-icon-close"><b>Закрыть</b></b> \
+					</a> \
 					<a class="b-graphics b-graphics-prev" href="?"><b></b></a> \
 					<a class="b-graphics b-graphics-next" href="?"><b></b></a> \
-				</div> \
-				<a class="b-close-modal" href="?"> \
-					<b class="b-icon b-icon-close"><b>Закрыть</b></b> \
-				</a>',
+					<div class="slide"> \
+						<img class="image"/> \
+						<em class="label"></em> \
+					</div> \
+				</div>',
 			singleClass: 'b-modal-gallery-single',
-			prevPrevSelector: '.prev-prev',
-			prevSelector: '.prev',
-			currentSelector: '.current',
-			nextSelector: '.next',
-			nextNextSelector: '.next-next',
+			slideSelector: '.slide',
 			imageSelector: '.image',
+			labelSelector: '.label',
 			wideImageClass: 'wide-image',
 			tallImageClass: 'tall-image',
 			nearImagesPosition: 266,
@@ -76,7 +62,7 @@ var modal = (function () {
 		_showLinks.live('click', function(event){
 			var thisLink = $(this);
 			
-			showContents(thisLink.attr('href'), thisLink.attr('rel'), thisLink);
+			showContents(thisLink.attr('href'), thisLink.attr('rel'), thisLink.attr('title'), thisLink);
 			showContainer();
 			
 			event.preventDefault();
@@ -98,7 +84,7 @@ var modal = (function () {
 		});
 	}
 	
-	function showContents(url, group, link){
+	function showContents(url, group, title, link){
 		var urlWithoutQuery = url,
 			queryPosition = url.indexOf('?'),
 			typeRegexp = /\.jpg$|\.jpeg$|\.png$|\.gif$|\.bmp$|\.html$|\.htm$/;
@@ -111,42 +97,45 @@ var modal = (function () {
 		
 		if( urlType == '.jpg' || urlType == '.jpeg' || urlType == '.png' || urlType == '.gif' ){
 			_gallery = {};
-			
 			_gallery.container = $(SETTINGS.gallery.html);
-			_gallery.current = $(SETTINGS.gallery.currentSelector, _gallery.container);
-			_gallery.currentImage = $(SETTINGS.gallery.imageSelector, _gallery.current);
 			
 			if( group ){
 				_showLinksGroup = _showLinks.filter('[rel=' + group + ']');
-				_gallery.currentIndex = _showLinksGroup.index(link);
+				_gallery.index = _showLinksGroup.index(link);
+				_gallery.slides = new Array(_showLinksGroup.length);
 				
-				_gallery.prevPrev = $(SETTINGS.gallery.prevPrevSelector, _gallery.container);
-				_gallery.prev = $(SETTINGS.gallery.prevSelector, _gallery.container);
-				_gallery.next = $(SETTINGS.gallery.nextSelector, _gallery.container);
-				_gallery.nextNext = $(SETTINGS.gallery.nextNextSelector, _gallery.container);
+				_gallery.slides[_gallery.index] = {
+					container: $(SETTINGS.gallery.slideSelector, _gallery.container),
+					image: $(SETTINGS.gallery.imageSelector, _gallery.container),
+					label: $(SETTINGS.gallery.labelSelector, _gallery.container)
+				}
+				_gallery.slideClone = _gallery.slides[_gallery.index].container.clone();
+				loadGalleryImage(_gallery.slides[_gallery.index], url, title, 'center');
 				
-				_gallery.prevPrevImage = $(SETTINGS.gallery.imageSelector, _gallery.prevPrev);
-				_gallery.prevImage = $(SETTINGS.gallery.imageSelector, _gallery.prev);
-				_gallery.nextImage = $(SETTINGS.gallery.imageSelector, _gallery.next);
-				_gallery.nextNextImage = $(SETTINGS.gallery.imageSelector, _gallery.nextNext);
+				loadGallerySlide(_gallery.index + 1);
+				loadGallerySlide(_gallery.index - 1);
+				loadGallerySlide(_gallery.index + 2);
+				loadGallerySlide(_gallery.index - 2);
 				
 				_gallery.prevControl = $(SETTINGS.gallery.prevControlSelector, _gallery.container);
 				_gallery.nextControl = $(SETTINGS.gallery.nextControlSelector, _gallery.container);
 				
-				loadGalleryNearImages();
-				
 				//manageGalleryControls();
+				assignGalleryEvents();
 			}
 			else{
+				_gallery.slide = {
+					container: $(SETTINGS.gallery.slideSelector, _gallery.container),
+					image: $(SETTINGS.gallery.imageSelector, _gallery.container),
+					label: $(SETTINGS.gallery.labelSelector, _gallery.container)
+				};
+				
+				loadGalleryImage(_gallery.slide, url, title, 'center');
+				
 				_gallery.container.addClass(SETTINGS.gallery.singleClass);
 			}
 			
-			assignGalleryEvents();
-			
-			_gallery.currentImage.hide().load(function(){
-				alignGalleryImage(_gallery.current, _gallery.currentImage, 'center');
-				_gallery.currentImage.show();
-			}).attr('src', url);
+			alignImagesOnResize();
 			
 			_container.append(_gallery.container);
 			_outer.hide();
@@ -164,85 +153,106 @@ var modal = (function () {
 		}
 	}
 	
-	function loadGalleryNearImages(){
-		if( _gallery.currentIndex < _showLinksGroup.length - 1 ){
-			_gallery.nextImage.hide().load(function(){
-				alignGalleryImage(_gallery.next, _gallery.nextImage, 'left');
-				_gallery.nextImage.show();
-			}).attr('src', _showLinksGroup.eq(_gallery.currentIndex + 1).attr('href'));
-		}
-		if( _gallery.currentIndex > 0 ){
-			_gallery.prevImage.load(function(){
-				alignGalleryImage(_gallery.prev, _gallery.prevImage, 'right');
-				_gallery.prevImage.show();
-			}).attr('src', _showLinksGroup.eq(_gallery.currentIndex - 1).attr('href'));
-		}
-		loadNextNextImage();
-		loadPrevPrevImage();
-	}
-	
-	function loadNextNextImage(){
-		if( _gallery.currentIndex < _showLinksGroup.length - 2 ){
-			_gallery.nextNextImage.load(function(){
-				_gallery.nextNextImage.removeClass(SETTINGS.gallery.wideImageClass).removeClass(SETTINGS.gallery.tallImageClass).css('margin', 0);
-				alignGalleryImage(_gallery.nextNext, _gallery.nextNextImage, 'left');
-				_gallery.nextNextImage.show();
-			}).attr('src', _showLinksGroup.eq(_gallery.currentIndex + 2).attr('href'));
+	function loadGallerySlide( index ){
+		if( index >= 0 && index < _gallery.slides.length && typeof _gallery[index] === 'undefined' ){
+			var imageAlign = (index > _gallery.index) ? 'left' : 'right',
+				slidePosition = (index - _gallery.index) + '00%';
+			
+			_gallery.slides[index] = {
+				container: _gallery.slideClone.clone()
+			}
+			
+			_gallery.slides[index].container.css('left', slidePosition).appendTo(_gallery.container);
+			_gallery.slides[index].image = $(SETTINGS.gallery.imageSelector, _gallery.slides[index].container);
+			_gallery.slides[index].label = $(SETTINGS.gallery.labelSelector, _gallery.slides[index].container);
+			
+			loadGalleryImage(_gallery.slides[index], _showLinksGroup.eq(index).attr('href'), _showLinksGroup.eq(index).attr('title'), imageAlign);
 		}
 	}
 	
-	function loadPrevPrevImage(){
-		if( _gallery.currentIndex > 1 ){
-			_gallery.prevPrevImage.load(function(){
-				_gallery.prevPrevImage.removeClass(SETTINGS.gallery.wideImageClass).removeClass(SETTINGS.gallery.tallImageClass).css('margin', 0);
-				alignGalleryImage(_gallery.prevPrev, _gallery.prevPrevImage, 'right');
-				_gallery.prevPrevImage.show();
-			}).attr('src', _showLinksGroup.eq(_gallery.currentIndex - 2).attr('href'));
-		}
+	function loadGalleryImage(slide, url, title, horizontalAlign){
+		slide.image
+			.hide()
+			.load(function(){
+				alignGalleryImage(slide.container, slide.image, horizontalAlign);
+				slide.image.show();
+				slide.label.text(title);
+			})
+			.attr('src', url);
 	}
 	
-	function alignGalleryImage(imageContainer, image, horizontalAlign){
-		var imageWidth = image.width(),
-			imageHeight = image.height(),
+	function alignGalleryImage(container, image, horizontalAlign){
+		var imageWidth = 0,
+			imageHeight = 0,
 			descreasingRatio = 1;
 		
-		if( imageContainer.width() / imageContainer.height() < imageWidth / imageHeight ){
-			if( imageContainer.width() < imageWidth ){
-				descreasingRatio = imageContainer.width() / imageWidth;
-				image.addClass(SETTINGS.gallery.wideImageClass);
+		if( typeof image.data('width') !== 'undefined' ){
+			imageWidth = image.data('width');
+			imageHeight = image.data('height');
+		}
+		else{
+			imageWidth = image.width();
+			imageHeight = image.height();
+			
+			image.data('width', imageWidth);
+			image.data('height', imageHeight);
+		}
+		
+		if( container.width() / container.height() < imageWidth / imageHeight ){
+			if( container.width() < imageWidth ){
+				descreasingRatio = container.width() / imageWidth;
+				image.removeClass(SETTINGS.gallery.tallImageClass).addClass(SETTINGS.gallery.wideImageClass);
 			}
 			else{
-				image.css('margin-left', (imageContainer.width() - imageWidth) / 2);
+				image.css('margin-left', (container.width() - imageWidth) / 2);
 			}
 			
 			if( horizontalAlign == 'left' ) {
 				image.css('margin-left', -SETTINGS.gallery.nearImagesPosition);
 			}
 			else if( horizontalAlign == 'right' ) {
-				image.css('margin-left', imageContainer.width() - imageWidth * descreasingRatio + SETTINGS.gallery.nearImagesPosition);
+				image.css('margin-left', container.width() - imageWidth * descreasingRatio + SETTINGS.gallery.nearImagesPosition);
 			}
 			
-			image.css('margin-top', (imageContainer.height() - imageHeight * descreasingRatio) / 2);
+			image.css('margin-top', (container.height() - imageHeight * descreasingRatio) / 2);
 		}
 		else{
-			if( imageContainer.height() < imageHeight ){
-				descreasingRatio = imageContainer.height() / imageHeight;
-				image.addClass(SETTINGS.gallery.tallImageClass);
+			if( container.height() < imageHeight ){
+				descreasingRatio = container.height() / imageHeight;
+				image.removeClass(SETTINGS.gallery.wideImageClass).addClass(SETTINGS.gallery.tallImageClass);
 			}
 			else{
-				image.css('margin-top', (imageContainer.height() - imageHeight ) / 2);
+				image.css('margin-top', (container.height() - imageHeight ) / 2);
 			}
 			
 			if( horizontalAlign == 'center' ){
-				image.css('margin-left', (imageContainer.width() - imageWidth * descreasingRatio) / 2);
+				image.css('margin-left', (container.width() - imageWidth * descreasingRatio) / 2);
 			}
 			else if( horizontalAlign == 'left' ) {
 				image.css('margin-left', -SETTINGS.gallery.nearImagesPosition);
 			}
 			else if( horizontalAlign == 'right' ) {
-				image.css('margin-left', imageContainer.width() - imageWidth * descreasingRatio + SETTINGS.gallery.nearImagesPosition);
+				image.css('margin-left', container.width() - imageWidth * descreasingRatio + SETTINGS.gallery.nearImagesPosition);
 			}
 		}
+	}
+	
+	function alignImagesOnResize(){
+		$(window).resize(function(){
+			if( typeof _gallery.slide === 'object' ){
+			}
+			else if( typeof _gallery.slides === 'object' ){
+				// Полная жесть с выравниванием
+				alignGalleryImage(_gallery.slides[_gallery.index].container, _gallery.slides[_gallery.index].image, 'center');
+				
+				if( _gallery.index > 0 ){
+					alignGalleryImage(_gallery.slides[_gallery.index - 1].container, _gallery.slides[_gallery.index - 1].image, 'right');
+				}
+				if( _gallery.index < _gallery.slides.length - 1 ){
+					alignGalleryImage(_gallery.slides[_gallery.index + 1].container, _gallery.slides[_gallery.index + 1].image, 'left');
+				}
+			}
+		});
 	}
 	
 	/*
@@ -270,43 +280,29 @@ var modal = (function () {
 	
 	function assignGalleryEvents(){
 		_gallery.prevControl.click(function(event){
-			if( _gallery.currentIndex > 0 && !_gallery.container.data('animating') ){
+			if( _gallery.index > 0 && !_gallery.container.data('animating') ){
 				_gallery.container.data('animating', true);
-				_gallery.currentIndex--;
 				
-				_gallery.prevPrev.animate({ left: '-100%' }, SETTINGS.gallery.animationTime);
-				_gallery.prev.animate({ left: '0' }, SETTINGS.gallery.animationTime);
-				_gallery.current.animate({ left: '100%' }, SETTINGS.gallery.animationTime);
+				if( _gallery.index > 1 ){
+					_gallery.slides[_gallery.index - 2].container.animate({ left: '-100%' }, SETTINGS.gallery.animationTime);
+				}
+				_gallery.slides[_gallery.index - 1].container.animate({ left: '0' }, SETTINGS.gallery.animationTime);
+				_gallery.slides[_gallery.index].container.animate({ left: '100%' }, SETTINGS.gallery.animationTime);
 				
-				_gallery.prevImage.animate({
-					marginLeft: (_gallery.prev.width() - _gallery.prevImage.width()) / 2
+				_gallery.slides[_gallery.index - 1].image.animate({
+					marginLeft: (_gallery.slides[_gallery.index - 1].container.width() - _gallery.slides[_gallery.index - 1].image.width()) / 2
 				}, SETTINGS.gallery.animationTime);
 				
-				_gallery.currentImage.animate({
+				if( _gallery.index < _gallery.slides.length - 1 ){
+					_gallery.slides[_gallery.index + 1].container.animate({ left: '200%' }, SETTINGS.gallery.animationTime);
+				}
+				
+				_gallery.slides[_gallery.index].image.animate({
 					marginLeft: -SETTINGS.gallery.nearImagesPosition
-				}, SETTINGS.gallery.animationTime);
-				
-				_gallery.next.animate({
-					left: '200%'
 				}, SETTINGS.gallery.animationTime, function(){
-					var tempContainer = _gallery.nextNext,
-						tempImage = _gallery.nextNextImage;
+					loadGallerySlide(_gallery.index - 3);
 					
-					_gallery.nextNext = _gallery.next;
-					_gallery.next = _gallery.current;
-					_gallery.current = _gallery.prev;
-					_gallery.prev = _gallery.prevPrev;
-					_gallery.prevPrev = tempContainer;
-					
-					_gallery.nextNextImage = _gallery.nextImage;
-					_gallery.nextImage = _gallery.currentImage;
-					_gallery.currentImage = _gallery.prevImage;
-					_gallery.prevImage = _gallery.prevPrevImage;
-					_gallery.prevPrevImage = tempImage;
-					
-					_gallery.prevPrev.css('left', '-200%');
-					loadPrevPrevImage();
-					
+					_gallery.index--;
 					_gallery.container.data('animating', false);
 				});
 			}
@@ -315,43 +311,29 @@ var modal = (function () {
 		});
 		
 		_gallery.nextControl.click(function(event){
-			if( _gallery.currentIndex < _showLinksGroup.length - 1 && !_gallery.container.data('animating') ){
+			if( _gallery.index < _gallery.slides.length - 1 && !_gallery.container.data('animating') ){
 				_gallery.container.data('animating', true);
-				_gallery.currentIndex++;
 				
-				_gallery.prev.animate({ left: '-200%' }, SETTINGS.gallery.animationTime);
-				_gallery.current.animate({ left: '-100%' }, SETTINGS.gallery.animationTime);
-				_gallery.next.animate({ left: '0' }, SETTINGS.gallery.animationTime);
+				if( _gallery.index > 0 ){
+					_gallery.slides[_gallery.index - 1].container.animate({ left: '-200%' }, SETTINGS.gallery.animationTime);
+				}
+				_gallery.slides[_gallery.index].container.animate({ left: '-100%' }, SETTINGS.gallery.animationTime);
+				_gallery.slides[_gallery.index + 1].container.animate({ left: '0' }, SETTINGS.gallery.animationTime);
 				
-				_gallery.currentImage.animate({
-					marginLeft: _gallery.current.width() - _gallery.currentImage.width() + SETTINGS.gallery.nearImagesPosition
+				_gallery.slides[_gallery.index].image.animate({
+					marginLeft: _gallery.slides[_gallery.index].container.width() - _gallery.slides[_gallery.index].image.width() + SETTINGS.gallery.nearImagesPosition
 				}, SETTINGS.gallery.animationTime);
 				
-				_gallery.nextImage.animate({
-					marginLeft: (_gallery.next.width() - _gallery.nextImage.width()) / 2
-				}, SETTINGS.gallery.animationTime);
+				if( _gallery.index < _gallery.slides.length - 2 ){
+					_gallery.slides[_gallery.index + 2].container.animate({ left: '100%' }, SETTINGS.gallery.animationTime);
+				}
 				
-				_gallery.nextNext.animate({
-					left: '100%'
+				_gallery.slides[_gallery.index + 1].image.animate({
+					marginLeft: (_gallery.slides[_gallery.index + 1].container.width() - _gallery.slides[_gallery.index + 1].image.width()) / 2
 				}, SETTINGS.gallery.animationTime, function(){
-					var tempContainer = _gallery.prevPrev,
-						tempImage = _gallery.prevPrevImage;
+					loadGallerySlide(_gallery.index + 3);
 					
-					_gallery.prevPrev = _gallery.prev;
-					_gallery.prev = _gallery.current;
-					_gallery.current = _gallery.next;
-					_gallery.next = _gallery.nextNext;
-					_gallery.nextNext = tempContainer;
-					
-					_gallery.prevPrevImage = _gallery.prevImage;
-					_gallery.prevImage = _gallery.currentImage;
-					_gallery.currentImage = _gallery.nextImage;
-					_gallery.nextImage = _gallery.nextNextImage;
-					_gallery.nextNextImage = tempImage;
-					
-					_gallery.nextNext.css('left', '200%');
-					loadNextNextImage();
-					
+					_gallery.index++;
 					_gallery.container.data('animating', false);
 				});
 			}
@@ -360,18 +342,11 @@ var modal = (function () {
 		});
 	}
 	
-	function switchGalleryImage(){
-		//_gallery.mainImage.attr('src', _showLinksGroup.eq(_currentLink).attr('href') );
-		//manageGalleryControls();
-	}
-	
 	function clearContents(){
 		if( typeof _inlineContainer === 'object' && _inlineContainer.hasOwnProperty('length') && _inlineContainer.length ){
 			_inlineContainer.append(_inner.children());
 			_inlineContainer = '';
 		}
-		
-		
 		if( typeof _gallery === 'object' && _gallery.hasOwnProperty('container') ){
 			_gallery.container.remove();
 			_outer.show();
